43ff49c8f83b7f82715258ab8911b26c26f94073
1chary <abhilashchary04@gmail.com>
2024-11-12 15:11:31 +0530

    added services and connect component

--- myApp/src/App.jsx
+++ myApp/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import Home from './components/Home'
 import About from './components/About'
+import Services from './components/Services'
+import Connect from './components/Connect'
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Routes>
         <Route path = "/" element = {<Home/>} />
         <Route exact path = "/about" element = {<About/>} />
+        <Route exact path = "/services" element = {<Services/>} />
+        <Route exact path = "/connect" element = {<Connect/>} />
       </Routes>
     </div>
   )
--- myApp/src/components/Header/index.jsx
+++ myApp/src/components/Header/index.jsx
@@ -10,10 +10,17 @@ const Header = () => {
             <Link to = "/" className='elements-style'>  
                 <h3 className="options-style">Home</h3>
             </Link>
-            <Link to = "/about">
+            <Link to = "/about" className='elements-style'>
                 <h3 className="options-style">About</h3>
             </Link>
+            <Link to = "/services" className='elements-style'>  
+                <h3 className="options-style">Services</h3>
+            </Link>
+            <Link to = "/connect" className='elements-style'>
+                <h3 className="options-style">Connect</h3>
+            </Link>
         </div>
+        <button className="connect-with-me-button-style">Connect With Me</button>
     </div>
   )
 }
